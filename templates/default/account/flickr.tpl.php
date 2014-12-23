eeed422d0b74e77a945c761c61aaa1f326fbf39e
Erin Jo Richey <richey.erin@gmail.com>
2014-12-23 13:18:41 -0800

    Formatting changes for account page

--- templates/default/account/flickr.tpl.php
+++ templates/default/account/flickr.tpl.php
@@ -1,8 +1,8 @@
 <div class="row">
 
     <div class="span10 offset1">
+	            <?=$this->draw('account/menu')?>
         <h1>Flickr</h1>
-        <?= $this->draw('account/menu') ?>
     </div>
 
 </div>
@@ -11,110 +11,53 @@
         <form action="/account/flickr/" class="form-horizontal" method="post">
             <?php
                 if (empty(\Idno\Core\site()->session()->currentUser()->flickr)) {
-                    ?>
+            ?>
                     <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                If you have a Flickr account, you may connect it here. Public content that you
-                                post to this site will be automatically cross-posted to your Flickr wall.
+                        <div class="controls-config">
+	                       <div class="row">
+						   		<div class="span6">
+                                                            <p>
+                                Easily share pictures to Flickr.</p>  
+                                
+                                <p>
+                                With Flickr connected, you can cross-post images that you publish publicly on your site. 
                             </p>
-                            <?php
-
-                                if (empty($vars['login_url'])) {
-                                    if (\Idno\Core\site()->session()->isAdmin()) {
-
-                                        ?>
-                                        <p>
-                                            Before you can connect to Flickr, you need to set up your API details.
-                                            <a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>admin/flickr/">Click
-                                                here to get started.</a>
-                                        </p>
-                                    <?php
-
-                                    } else {
-
-                                        ?>
-                                        <p>
-                                            You can't connect to Flickr right now. Please try again later.
-                                        </p>
-                                    <?php
-                                    }
-                                } else {
-
-                                    ?>
-                                    <p>
-                                        <a href="<?= $vars['login_url'] ?>" class="btn btn-large btn-success">Click here
-                                            to connect Flickr to your account</a>
-                                    </p>
-                                <?php
-
-                                }
-
-
-                            ?>
+						   		</div>
+	                       </div>
+	                       	                        <div class="social span4">
+                            <p>
+                                <a href="<?=$vars['login_url']?>" class="connect fl">Connect Flickr</a>
+                            </p>
+	                       	                        </div>
                         </div>
                     </div>
-                <?php
-
-                } else if (!\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
-
-                    ?>
-                    <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                Your account is currently connected to Flickr. Public content that you post here
-                                will be shared with your Flickr account.
-                            </p>
-
-                            <p>
-                                <input type="hidden" name="remove" value="1"/>
-                                <button type="submit" class="btn btn-large btn-primary">Click here to remove Flickr from
-                                    your account.
-                                </button>
-                            </p>
-                        </div>
-                    </div>
-
                 <?php
 
                 } else {
 
-            ?>
+                    ?>
                     <div class="control-group">
-                        <div class="controls">
-                            <p class="explanation">
-                                You have connected the following accounts to Flickr:
-                            </p>
-                            <?php
-
-                                if ($accounts = \Idno\Core\site()->syndication()->getServiceAccounts('flickr')) {
-
-                                    foreach ($accounts as $account) {
-
-                                        ?>
-                                        <p>
-                                            <input type="hidden" name="remove" value="<?= $account['username'] ?>"/>
-                                            <button type="submit"
-                                                    class="btn btn-primary"><?= $account['username'] ?></button>
-                                        </p>
-                                    <?php
-
-                                    }
-
-                                }
-
-                            ?>
+                        <div class="controls-config">
+	                      <div class="row">
+						    <div class="span6">
                             <p>
-                                <a href="<?= $vars['login_url'] ?>" class="">Click here
-                                    to connect another SoundCloud account</a>
+                                Your account is currently connected to Flickr. Public content that you post here
+                                can be shared with your Flickr account.
+                            </p>
+						    </div>
+	                      </div>
+                            <p>
+                                <input type="hidden" name="remove" value="1" />
+                                <button type="submit" class="btn btn-large btn-primary">Disconnect Flickr.</button>
                             </p>
                         </div>
                     </div>
-            <?php
+
+                <?php
 
                 }
             ?>
-            <?= \Idno\Core\site()->actions()->signForm('/account/flickr/') ?>
+            <?= \Idno\Core\site()->actions()->signForm('/account/flickr/')?>
         </form>
     </div>
 </div>
