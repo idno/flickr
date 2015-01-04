a632e9f7a80266838f5e2d48aa00201b46239066
Ben Werdmuller <ben@benwerd.com>
2015-01-03 19:48:35 -0800

    Improved account settings page

--- templates/default/account/flickr.tpl.php
+++ templates/default/account/flickr.tpl.php
@@ -1,36 +1,61 @@
 <div class="row">
 
     <div class="span10 offset1">
-	            <?=$this->draw('account/menu')?>
+        <?= $this->draw('account/menu') ?>
         <h1>Flickr</h1>
     </div>
 
 </div>
 <div class="row">
     <div class="span10 offset1">
-        <form action="/account/flickr/" class="form-horizontal" method="post">
+        <form action="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/flickr/" class="form-horizontal" method="post">
             <?php
                 if (empty(\Idno\Core\site()->session()->currentUser()->flickr)) {
-            ?>
+                    ?>
                     <div class="control-group">
                         <div class="controls-config">
-	                       <div class="row">
-						   		<div class="span6">
-                                                            <p>
-                                Easily share pictures to Flickr.</p>  
-                                
+                            <div class="row">
+                                <div class="span6">
+                                    <p>
+                                        Easily share pictures to Flickr.</p>
+
+                                    <p>
+                                        With Flickr connected, you can cross-post images that you publish publicly on
+                                        your site.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="social span4">
                                 <p>
-                                With Flickr connected, you can cross-post images that you publish publicly on your site. 
-                            </p>
-						   		</div>
-	                       </div>
-	                       	                        <div class="social span4">
-                            <p>
-                                <a href="<?=$vars['login_url']?>" class="connect fl">Connect Flickr</a>
-                            </p>
-	                       	                        </div>
+                                    <a href="<?= $vars['login_url'] ?>" class="connect fl">Connect Flickr</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
+                <?php
+
+                } else if (!\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
+
+                    ?>
+                    <div class="control-group">
+                        <div class="controls-config">
+                            <div class="row">
+                                <div class="span6">
+                                    <p>
+                                        Your account is currently connected to Flickr. Public content that you post here
+                                        can be shared with your Flickr account.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <input type="hidden" name="remove" value="1"/>
+                                    <button type="submit" class="connect fl connected">Disconnect Flickr</button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
                 <?php
 
                 } else {
@@ -38,28 +63,45 @@
                     ?>
                     <div class="control-group">
                         <div class="controls-config">
-	                      <div class="row">
-						    <div class="span6">
+                            <div class="row">
+                                <div class="span6">
+                                    <p>
+                                        You have connected the following accounts to Flickr. Public content that you
+                                        post here
+                                        can be shared with your Flickr accounts.
+                                    </p>
+                                </div>
+                            </div>
+                            <?php
+
+                                if ($accounts = \Idno\Core\site()->syndication()->getServiceAccounts('flickr')) {
+
+                                    foreach ($accounts as $account) {
+
+                                        ?>
+                                        <p>
+                                            <input type="hidden" name="remove" value="<?= $account['username'] ?>"/>
+                                            <button type="submit"
+                                                    class="connect fl connected"><?= $account['username'] ?></button>
+                                        </p>
+                                    <?php
+
+                                    }
+
+                                }
+
+                            ?>
                             <p>
-                                Your account is currently connected to Flickr. Public content that you post here
-                                can be shared with your Flickr account.
+                                <a href="<?= $vars['login_url'] ?>" class="">Click here
+                                    to connect another Flickr account</a>
                             </p>
-						    </div>
-	                      </div>
-	                      <div class="social">
-                            <p>
-                                <input type="hidden" name="remove" value="1" />
-                                <button type="submit" class="connect fl connected">Disconnect Flickr</button>
-                            </p>
-	                      </div>
                         </div>
                     </div>
-
                 <?php
 
                 }
             ?>
-            <?= \Idno\Core\site()->actions()->signForm('/account/flickr/')?>
+            <?= \Idno\Core\site()->actions()->signForm('/account/flickr/') ?>
         </form>
     </div>
 </div>
