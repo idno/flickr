acf450cdca7f388b9c30bc9b8a1c98c45766c598
C <cn@chrs.nl>
2018-09-19 13:48:44 +0000

    Pared down further.

--- Pages/Callback.php
+++ Pages/Callback.php
@@ -17,14 +17,9 @@
                 $this->gatekeeper(); // Logged-in users only
                 if ($flickr = \Idno\Core\Idno::site()->plugins()->get('Flickr')) {
 
-                    // https://hasin.me/2012/03/07/connecting-to-flickr-using-pecl-oauth-extension/
-
-                    $oauth['flickr']['requesttokenurl'] = "https://www.flickr.com/services/oauth/request_token";
-                    $oauth['flickr']['accesstokenurl']  = "https://www.flickr.com/services/oauth/access_token";
-                    $oauth['flickr']['authurl']         = "https://www.flickr.com/services/oauth/authorize";
-
                     $api_key = \Idno\Core\Idno::site()->config()->flickr['apiKey'];
                     $api_secret = \Idno\Core\Idno::site()->config()->flickr['secret'];
+
                     $callback = \Idno\Core\Idno::site()->config()->getURL() . 'flickr/callback';
 
                     require_once(dirname(__FILE__) . '/../external/DPZ/Flickr.php');
@@ -35,35 +30,16 @@
                     if(empty($_SESSION['faccess_oauth_token'])) {
 
                         if(!$flickr->authenticate('write')) die('Laughing');
-                        $access_token_info = $flickr->authenticate('write');
 
-                        $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
-                        $userName = $flickr->getOauthData(\DPZ\Flickr::USER_NAME);
-                        $userFullName = $flickr->getOauthData(\DPZ\Flickr::USER_FULL_NAME);
+                            // $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
+                            $userName = $flickr->getOauthData(\DPZ\Flickr::USER_NAME);
+                            $userFullName = $flickr->getOauthData(\DPZ\Flickr::USER_FULL_NAME);
 
-                        if($access_token_info) {
-                            $_SESSION['faccess_oauth_token'] = $flickr->getOauthData('oauth_access_token');
-                            error_log('oauth_access_token: ' . $_SESSION['faccess_oauth_token'] . ' ;');
-                            $_SESSION['faccess_oauth_token_secret'] = $flickr->getOauthData('oauth_access_token_secret');
-                        }
-                    }
+                          $result['fullname'] = $userFullName;
+                          $result['username'] = $userName;
 
-                    // authenticated
-                    if(isset($_SESSION['faccess_oauth_token'])) {
-                        //now fetch current user's profile
-                        $access_token = $_SESSION['faccess_oauth_token'];
-                        $access_token_secret = $_SESSION['faccess_oauth_token_secret'];
-
-                        $response = $flickr->call('flickr.auth.oauth.checkToken');
-
-                        if (@$response['stat'] === 'ok')
-                        {
-                          $result['fullname'] = $response['oauth']['user']['fullname'];
-                          $result['username'] = $response['oauth']['user']['username'];
-                        }
-
-                         $result['token'] = $_SESSION['faccess_oauth_token'];
-                        $result['secret'] = $_SESSION['faccess_oauth_token_secret'];
+                         $result['token'] = $flickr->getOauthData('oauth_access_token');
+                        $result['secret'] = $flickr->getOauthData('oauth_access_token_secret');
                     }
 
                     if (!empty($result['token'])) {
