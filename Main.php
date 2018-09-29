008973640bb3490d5b88e9bc831f8e3a64b662c5
C <cn@chrs.nl>
2018-09-29 14:40:02 +0000

    Address PHP CS Errors

--- Main.php
+++ Main.php
@@ -1,45 +1,91 @@
 <?php
 
-    namespace IdnoPlugins\Flickr {
+    /**
+     * Flickr pages
+     *
+     * PHP version 5.6
+     *
+     * @category Plug-in
+     * @package  Photo_POSSE
+     * @author   Known, Inc <hello@withknown.com>
+     * @license  https://github.com/idno/Known/blob/master/LICENSE Apache
+     * @link     https://github.com/Idno/Flickr
+     */
 
-        class Main extends \Idno\Common\Plugin {
-	    
-	    function registerTranslations() {
+namespace IdnoPlugins\Flickr {
+
+    /**
+     * Default class to serve Flickr-related account actions
+     *
+     * @category Class
+     * @package  Flickr
+     * @author   @cdn <cn@domain.tld>
+     * @license  https://github.com/idno/Flickr/blob/master/LICENCE Above
+     * @link     https://github.com/cdn
+     */
+    class Main extends \Idno\Common\Plugin
+    {
+
+        /**
+         * Setup plug-in l10n
+         *
+         * @return not applicable
+         */
+        function registerTranslations()
+        {
 
                 \Idno\Core\Idno::site()->language()->register(
                     new \Idno\Core\GetTextTranslation(
                         'flickr', dirname(__FILE__) . '/languages/'
                     )
                 );
-            }
+        }
 
-            function registerPages() {
-                // Deauth URL
-                    \Idno\Core\Idno::site()->addPageHandler('flickr/deauth','\IdnoPlugins\Flickr\Pages\Deauth',true);
-                // Register the callback URL
-                    \Idno\Core\Idno::site()->addPageHandler('flickr/callback','\IdnoPlugins\Flickr\Pages\Callback',true);
-                // Register admin settings
-                    \Idno\Core\Idno::site()->addPageHandler('admin/flickr','\IdnoPlugins\Flickr\Pages\Admin');
-                // Register settings page
-                    \Idno\Core\Idno::site()->addPageHandler('account/flickr','\IdnoPlugins\Flickr\Pages\Account');
+        /**
+         * Setup page handling
+         *
+         * @return not applicable
+         */
+        function registerPages()
+        {
+            // Deauth URL
+                \Idno\Core\Idno::site()->addPageHandler('flickr/deauth', '\IdnoPlugins\Flickr\Pages\Deauth', true);
+            // Register the callback URL
+                \Idno\Core\Idno::site()->addPageHandler('flickr/callback', '\IdnoPlugins\Flickr\Pages\Callback', true);
+            // Register admin settings
+                \Idno\Core\Idno::site()->addPageHandler('admin/flickr', '\IdnoPlugins\Flickr\Pages\Admin');
+            // Register settings page
+                \Idno\Core\Idno::site()->addPageHandler('account/flickr', '\IdnoPlugins\Flickr\Pages\Account');
 
-                /** Template extensions */
-                // Add menu items to account & administration screens
-                    \Idno\Core\Idno::site()->template()->extendTemplate('admin/menu/items','admin/flickr/menu');
-                    \Idno\Core\Idno::site()->template()->extendTemplate('account/menu/items','account/flickr/menu');
-                    \Idno\Core\Idno::site()->template()->extendTemplate('onboarding/connect/networks','onboarding/connect/flickr');
-            }
+            /**
+              * Template extensions
+              */
 
-            function registerEventHooks() {
+            // Add menu items to account & administration screens
+                \Idno\Core\Idno::site()->template()->extendTemplate('admin/menu/items', 'admin/flickr/menu');
+                \Idno\Core\Idno::site()->template()->extendTemplate('account/menu/items', 'account/flickr/menu');
+                \Idno\Core\Idno::site()->template()->extendTemplate('onboarding/connect/networks', 'onboarding/connect/flickr');
+        }
 
-                \Idno\Core\Idno::site()->syndication()->registerService('flickr', function() {
-                    return $this->hasFlickr();
-                }, array('image'));
+        /**
+         * Setup actions
+         *
+         * @return via another function
+         */
+        function registerEventHooks()
+        {
 
-                \Idno\Core\Idno::site()->addEventHook('user/auth/success', function(\Idno\Core\Event $event) {
+            \Idno\Core\Idno::site()->syndication()->registerService(
+                'flickr', function () {
+                        return $this->hasFlickr();
+                }, array('image')
+            );
+
+            \Idno\Core\Idno::site()->addEventHook(
+                'user/auth/success', function (\Idno\Core\Event $event) {
                     if ($this->hasFlickr()) {
                         if (is_array(\Idno\Core\Idno::site()->session()->currentUser()->flickr)) {
-                            foreach(\Idno\Core\Idno::site()->session()->currentUser()->flickr as $username => $details) {
+                            foreach (\Idno\Core\Idno::site()->session()->currentUser()->flickr as $username => $details) {
                                 if (!in_array($username, ['access_token','username'])) {
                                     \Idno\Core\Idno::site()->syndication()->registerServiceAccount('flickr', $username, $details['username']);
                                 }
@@ -49,18 +95,20 @@
                             }
                         }
                     }
-                });
+                }
+            );
 
-                // Push "images" to Flickr
-                \Idno\Core\Idno::site()->addEventHook('post/image/flickr',function(\Idno\Core\Event $event) {
-                    $eventdata = $event->data();
-                    $object = $eventdata['object'];
+            // Push "images" to Flickr
+            \Idno\Core\Idno::site()->addEventHook(
+                'post/image/flickr', function (\Idno\Core\Event $event) {
+                        $eventdata = $event->data();
+                        $object = $eventdata['object'];
                     if ($attachments = $object->getAttachments()) {
-                        foreach($attachments as $attachment) {
+                        foreach ($attachments as $attachment) {
                             if ($this->hasFlickr()) {
-				
-				\Idno\Core\Idno::site()->logging()->debug('Posting image to flickr...');
-				
+
+                                \Idno\Core\Idno::site()->logging()->debug('Posting image to flickr...');
+
                                 if (!empty($eventdata['syndication_account'])) {
                                     $flickrAPI  = $this->connect($eventdata['syndication_account']);
                                     $user_details = \Idno\Core\Idno::site()->session()->currentUser()->flickr[$eventdata['syndication_account']];
@@ -77,12 +125,10 @@
 
                                 if (!$flickrAPI) {
                                     \Idno\Core\Idno::site()->logging()->error('Failed to connect to Flickr API');
-                                }
-                                else if (empty($user_details)) {
+                                } else if (empty($user_details)) {
                                     \Idno\Core\Idno::site()->logging()->error('Failed to get user_details');
-                                }
-                                else {
-                                    $tags = str_replace('#','',implode(' ', $object->getTags())); // Get string of non-hashtagged tags
+                                } else {
+                                    $tags = str_replace('#', '', implode(' ', $object->getTags())); // Get string of non-hashtagged tags
                                     try {
 
                                         $title = html_entity_decode($object->getTitle());
@@ -95,43 +141,58 @@
                                             $filename = tempnam(sys_get_temp_dir(), 'idnoflickr');
                                             file_put_contents($filename, $bytes);
 
-                                            if(version_compare(phpversion(), '5.5', '>=')) {
+                                            if (version_compare(phpversion(), '5.5', '>=')) {
                                                 $params['photo'] = new \CURLFile($filename);
                                             } else {
                                                 $params['photo'] = '@'.$filename;
                                             }
 
                                             $info = filesize($params['photo']);
-                                            if($title)       $params['title']       = $title;
-                                            if($description) $params['description'] = $description;
-                                            if($tags)        $params['tags']        = $tags;  // Space-separated string
-                                            if($perms) {
-                                                if(isset($perms['is_public'])) $params['is_public'] = $perms['is_public'];
-                                                if(isset($perms['is_friend'])) $params['is_friend'] = $perms['is_friend'];
-                                                if(isset($perms['is_family'])) $params['is_family'] = $perms['is_family'];
+                                            if ($title) {
+                                                $params['title']       = $title;
+                                            }
+                                            if ($description) {
+                                                $params['description'] = $description;
+                                            }
+                                            if ($tags) {
+                                                $params['tags']        = $tags;  // Space-separated string
+                                            }
+                                            if ($perms) {
+                                                if (isset($perms['is_public'])) {
+                                                    $params['is_public'] = $perms['is_public'];
+                                                }
+                                                if (isset($perms['is_friend'])) {
+                                                    $params['is_friend'] = $perms['is_friend'];
+                                                }
+                                                if (isset($perms['is_family'])) {
+                                                    $params['is_family'] = $perms['is_family'];
+                                                }
                                             }
 
-                                            if($async)       $params['async']       = $async;
+                                            if ($async) {
+                                                $params['async']       = $async;
+                                            }
 
                                             $photo_id = $flickrAPI->upload($params);
-					    
-					    \Idno\Core\Idno::site()->logging()->debug('Flickr responded with ' . print_r($photo_id, true));
+
+                                            \Idno\Core\Idno::site()->logging()->debug('Flickr responded with ' . print_r($photo_id, true));
 
                                             $ok = @$photo_id['stat'];
 
                                             if ($ok == 'ok') {
-                                                $photo = $flickrAPI->call('flickr.photos.getInfo',
-                                                                          array('photo_id' => $photo_id['photoid']['_content']));
+                                                $photo = $flickrAPI->call(
+                                                    'flickr.photos.getInfo',
+                                                    array('photo_id' => $photo_id['photoid']['_content'])
+                                                );
 
-						if ($photo['photo']['urls']['url'][0]['type'] == 'photopage') {
-						    $object->setPosseLink('flickr',$photo['photo']['urls']['url'][0]['_content'], $name, $photo_id['photoid']['_content'], $photo['photo']['owner']['username']);
-						    $object->save();
-						}
-						\Idno\Core\Idno::site()->logging()->log($photo_id['photoid']['_content'] . ' pushed to Flickr.');
-					    }
-					    else {
-						\Idno\Core\Idno::site()->logging()->error("Failed to upload image to Flickr. code={$flickrAPI->getErrorCode()}, error={$flickrAPI->getErrorMessage()}");
-					    }
+                                                if ($photo['photo']['urls']['url'][0]['type'] == 'photopage') {
+                                                    $object->setPosseLink('flickr', $photo['photo']['urls']['url'][0]['_content'], $name, $photo_id['photoid']['_content'], $photo['photo']['owner']['username']);
+                                                    $object->save();
+                                                }
+                                                \Idno\Core\Idno::site()->logging()->log($photo_id['photoid']['_content'] . ' pushed to Flickr.');
+                                            } else {
+                                                \Idno\Core\Idno::site()->logging()->error("Failed to upload image to Flickr. code={$flickrAPI->getErrorCode()}, error={$flickrAPI->getErrorMessage()}");
+                                            }
 
                                         }
 
@@ -143,69 +204,85 @@
                             }
                         }
                     }
-                });
-            }
-
-            /**
-             * Return the URL required to authenticate with the API
-             * @return string
-             */
-            function getAuthURL() {
-
-                $login_url = \Idno\Core\Idno::site()->config()->getURL() . 'flickr/callback';
-                return $login_url;
-
-            }
-
-            /**
-             * Connect to Flickr
-             * @return bool|\Flickr
-             */
-            function connect($username = false) {
-                if (!empty(\Idno\Core\Idno::site()->config()->flickr)) {
-                    require_once(dirname(__FILE__) . '/external/DPZ/Flickr.php');
-                    $flickr = new \DPZ\Flickr(\Idno\Core\Idno::site()->config()->flickr['apiKey'],
-                                              \Idno\Core\Idno::site()->config()->flickr['secret']);
-                    if ($this->hasFlickr()) {
-                        if (empty($username)) {
-                            if (!empty(\Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'])) {
-                                // https://github.com/dopiaza/DPZFlickr/pull/8 https://github.com/lucasgd/DPZFlickr
-
-                                $flickr->isValidOauthToken(\Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'],
-                                                           \Idno\Core\Idno::site()->session()->currentUser()->flickr['secret']);
-                            }
-                        } else {
-                            if (!empty(
-                                \Idno\Core\Idno::site()->session()->currentUser()->flickr[$username]
-                            )) {
-                                $flickr->isValidOauthToken(\Idno\Core\Idno::site()->session()->currentUser()->flickr[$username]['access_token'],
-                                                           \Idno\Core\Idno::site()->session()->currentUser()->flickr[$username]['secret']);
-                            } else if (!empty(\Idno\Core\Idno::site()->session()->currentUser()->flickr['username']) && $username == \Idno\Core\Idno::site()->session()->currentUser()->flickr['username']) {
-                                $flickr->isValidOauthToken(\Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'],
-                                                           \Idno\Core\Idno::site()->session()->currentUser()->flickr['secret']);
-                            }
-                        }
-                    }
-
-                    return $flickr;
                 }
-                return false;
-            }
+            );
+        }
 
-            /**
-             * Can the current user use Flickr?
-             * @return bool
-             */
-            function hasFlickr() {
-                if (!\Idno\Core\Idno::site()->session()->currentUser()) {
-                    return false;
-                }
-                if (\Idno\Core\Idno::site()->session()->currentUser()->flickr) {
-                    return true;
-                }
-                return false;
-            }
+        /**
+         * Return the URL required to authenticate with the API
+         *
+         * @return string
+         */
+        function getAuthURL()
+        {
+
+            $login_url = \Idno\Core\Idno::site()->config()->getURL() . 'flickr/callback';
+            return $login_url;
 
         }
 
+        /**
+         * Connect to Flickr
+         *
+         * @param Username $username of Known/Flickr user
+         *
+         * @return bool|\Flickr
+         */
+        function connect($username = false)
+        {
+            if (!empty(\Idno\Core\Idno::site()->config()->flickr)) {
+                include_once dirname(__FILE__) . '/external/DPZ/Flickr.php';
+                $flickr = new \DPZ\Flickr(
+                    \Idno\Core\Idno::site()->config()->flickr['apiKey'],
+                    \Idno\Core\Idno::site()->config()->flickr['secret']
+                );
+                if ($this->hasFlickr()) {
+                    if (empty($username)) {
+                        if (!empty(\Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'])) {
+                            // https://github.com/dopiaza/DPZFlickr/pull/8 https://github.com/lucasgd/DPZFlickr
+
+                            $flickr->isValidOauthToken(
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'],
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr['secret']
+                            );
+                        }
+                    } else {
+                        if (!empty(\Idno\Core\Idno::site()->session()->currentUser()->flickr[$username])
+                        ) {
+                            $flickr->isValidOauthToken(
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr[$username]['access_token'],
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr[$username]['secret']
+                            );
+                        } else if (!empty(\Idno\Core\Idno::site()->session()->currentUser()->flickr['username']) && $username == \Idno\Core\Idno::site()->session()->currentUser()->flickr['username']) {
+                            $flickr->isValidOauthToken(
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr['access_token'],
+                                \Idno\Core\Idno::site()->session()->currentUser()->flickr['secret']
+                            );
+                        }
+                    }
+                }
+
+                return $flickr;
+            }
+            return false;
+        }
+
+        /**
+         * Can the current user use Flickr?
+         *
+         * @return bool
+         */
+        function hasFlickr()
+        {
+            if (!\Idno\Core\Idno::site()->session()->currentUser()) {
+                return false;
+            }
+            if (\Idno\Core\Idno::site()->session()->currentUser()->flickr) {
+                return true;
+            }
+            return false;
+        }
+
     }
+
+}
