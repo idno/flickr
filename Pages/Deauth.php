7f2947a9c597a22fcde513d832acc9162e69dc35
C <cn@chrs.nl>
2018-09-29 16:25:01 +0000

    Address PHP CS Errors

--- Pages/Deauth.php
+++ Pages/Deauth.php
@@ -2,45 +2,70 @@
 
     /**
      * Plugin administration
+     *
+     * PHP version 5.6
+     *
+     * @category Plug-in
+     * @package  Photo_POSSE
+     * @author   Known, Inc <hello@withknown.com>
+     * @license  https://github.com/idno/Known/blob/master/LICENSE Apache
+     * @link     https://github.com/Idno/Flickr
      */
 
-    namespace IdnoPlugins\Flickr\Pages {
+namespace IdnoPlugins\Flickr\Pages {
+
+    /**
+     * Default class to remove Flickr account credentials
+     *
+     * @category Class
+     * @package  Flickr
+     * @author   @cdn <cn@domain.tld>
+     * @license  https://github.com/idno/Flickr/blob/master/LICENCE Above
+     * @link     https://github.com/cdn
+     */
+    class Deauth extends \Idno\Common\Page
+    {
 
         /**
-         * Default class to serve the homepage
+         * HTTP GET Action
+         *
+         * @return of the Living Dead
          */
-        class Deauth extends \Idno\Common\Page
+        function getContent()
         {
-
-            function getContent()
-            {
-                $this->gatekeeper(); // Logged-in users only
-                if ($twitter = \Idno\Core\Idno::site()->plugins()->get('Flickr')) {
-                    if ($user = \Idno\Core\Idno::site()->session()->currentUser()) {
-                        if ($account = $this->getInput('remove')) {
-                            if (array_key_exists($account, $user->flickr)) {
-                                unset($user->flickr[$account]);
-                            } else {
-                                $user->flickr = false;
-                            }
+            $this->gatekeeper(); // Logged-in users only
+            if ($twitter = \Idno\Core\Idno::site()->plugins()->get('Flickr')) {
+                if ($user = \Idno\Core\Idno::site()->session()->currentUser()) {
+                    if ($account = $this->getInput('remove')) {
+                        if (array_key_exists($account, $user->flickr)) {
+                            unset($user->flickr[$account]);
                         } else {
                             $user->flickr = false;
                         }
-                        $user->save();
-                        \Idno\Core\Idno::site()->session()->refreshSessionUser($user);
-                        if (!empty($user->link_callback)) {
-                            error_log($user->link_callback);
-                            $this->forward($user->link_callback); exit;
-                        }
+                    } else {
+                        $user->flickr = false;
+                    }
+                    $user->save();
+                    \Idno\Core\Idno::site()->session()->refreshSessionUser($user);
+                    if (!empty($user->link_callback)) {
+                        error_log($user->link_callback);
+                        $this->forward($user->link_callback); exit;
                     }
                 }
-                $this->forward($_SERVER['HTTP_REFERER']);
             }
+            $this->forward($_SERVER['HTTP_REFERER']);
+        }
 
-            function postContent() {
-                $this->getContent();
-            }
-
+        /**
+         * HTTP GET Action
+         *
+         * @return this function returns nothing
+         */
+        function postContent()
+        {
+            $this->getContent();
         }
 
     }
+
+}
