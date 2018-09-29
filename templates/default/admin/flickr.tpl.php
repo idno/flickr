7126e9081989d3b841d61c06426a0f3bbc362044
C <cn@chrs.nl>
2018-09-29 17:15:51 +0000

    Address PHP CS Errors

--- templates/default/admin/flickr.tpl.php
+++ templates/default/admin/flickr.tpl.php
@@ -1,35 +1,49 @@
-<div class="row">
+<?php
+
+    /**
+     * Flickr pages
+     *
+     * PHP version 5.6
+     *
+     * @category Plug-in
+     * @package  Photo_POSSE
+     * @author   Known, Inc <hello@withknown.com>
+     * @license  https://github.com/idno/Known/blob/master/LICENSE Apache
+     * @link     https://github.com/Idno/Flickr
+     */
+
+?><div class="row">
 
     <div class="col-md-10 col-md-offset-1">
-	            <?=$this->draw('admin/menu')?>
-        <h1><?= \Idno\Core\Idno::site()->language()->_('Flickr configuration'); ?></h1>
+        <?php echo $this->draw('admin/menu')?>
+        <h1><?php echo \Idno\Core\Idno::site()->language()->_('Flickr configuration'); ?></h1>
     </div>
 
 </div>
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
-        <form action="<?=\Idno\Core\Idno::site()->config()->getURL()?>admin/flickr/" class="form-horizontal" method="post">
+        <form action="<?php echo \Idno\Core\Idno::site()->config()->getURL()?>admin/flickr/" class="form-horizontal" method="post">
             <div class="controls-group">
                 <div class="controls-config">
                     <p>
-                        <?= \Idno\Core\Idno::site()->language()->_('To begin using Flickr, <a href="http://www.flickr.com/services/apps/" target="_blank">create a new application in the Flickr apps portal</a>.'); ?></p>
+                        <?php echo \Idno\Core\Idno::site()->language()->_('To begin using Flickr, <a href="http://www.flickr.com/services/apps/" target="_blank">create a new application in the Flickr apps portal</a>.'); ?></p>
                     <p>
-                        <?= \Idno\Core\Idno::site()->language()->_('You need to edit the authentication flow and set the callback URL to:'); ?><br />
-                        <input type="text" class="form-control" value="<?=\Idno\Core\Idno::site()->config()->url . 'flickr/callback'?>" />
+                        <?php echo \Idno\Core\Idno::site()->language()->_('You need to edit the authentication flow and set the callback URL to:'); ?><br />
+                        <input type="text" class="form-control" value="<?php echo \Idno\Core\Idno::site()->config()->url . 'flickr/callback'?>" />
                     </p>
                 </div>
             </div>
             <div class="controls-group">
-	                <p>
-                        <?= \Idno\Core\Idno::site()->language()->_('Once you\'ve finished, fill in the details below. You can then <a href="%s">connect your Flickr account</a>.', [\Idno\Core\Idno::site()->config()->getURL() . "account/flickr/"]); ?>
+                    <p>
+                        <?php echo \Idno\Core\Idno::site()->language()->_('Once you\'ve finished, fill in the details below. You can then <a href="%s">connect your Flickr account</a>.', [\Idno\Core\Idno::site()->config()->getURL() . "account/flickr/"]); ?>
                     </p>
                 <label class="control-label" for="name">API Key</label>
-                    <input type="text" id="name" placeholder="API Key" class="form-control" name="apiKey" value="<?=htmlspecialchars(\Idno\Core\Idno::site()->config()->flickr['apiKey'])?>" >
+                    <input type="text" id="name" placeholder="API Key" class="form-control" name="apiKey" value="<?php echo htmlspecialchars(\Idno\Core\Idno::site()->config()->flickr['apiKey'])?>" >
                 <label class="control-label" for="name">App secret</label>
-                    <input type="text" id="name" placeholder="App secret" class="form-control" name="secret" value="<?=htmlspecialchars(\Idno\Core\Idno::site()->config()->flickr['secret'])?>" >
+                    <input type="text" id="name" placeholder="App secret" class="form-control" name="secret" value="<?php echo htmlspecialchars(\Idno\Core\Idno::site()->config()->flickr['secret'])?>" >
             </div>
           <div class="controls-group">
-	          <p>
+              <p>
                         After the Flickr application is configured, site users must authenticate their Flickr account under Settings.
                     </p>
 
@@ -39,7 +53,7 @@
                     <button type="submit" class="btn btn-primary">Save settings</button>
                 </div>
             </div>
-            <?= \Idno\Core\Idno::site()->actions()->signForm('/admin/flickr/')?>
+            <?php echo \Idno\Core\Idno::site()->actions()->signForm('/admin/flickr/')?>
         </form>
     </div>
 </div>
--- templates/default/admin/flickr/menu.tpl.php
+++ templates/default/admin/flickr/menu.tpl.php
@@ -1,1 +1,17 @@
-<li <?php if ($_SERVER['REQUEST_URI'] == '/admin/flickr/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/flickr/">Flickr</a></li>
+<?php
+
+    /**
+     * Flickr pages
+     *
+     * PHP version 5.6
+     *
+     * @category Plug-in
+     * @package  Photo_POSSE
+     * @author   Known, Inc <hello@withknown.com>
+     * @license  https://github.com/idno/Known/blob/master/LICENSE Apache
+     * @link     https://github.com/Idno/Flickr
+     */
+
+?><li <?php if ($_SERVER['REQUEST_URI'] == '/admin/flickr/') {
+    echo 'class="active"';
+} ?>><a href="<?php echo \Idno\Core\Idno::site()->config()->getDisplayURL()?>admin/flickr/">Flickr</a></li>
