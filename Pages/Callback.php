683f0983d7e90ed8765ac65c4f46ffcc84736fb7
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-11 15:43:06 +0100

    Save nsid of user, as some plugins require it

--- Pages/Callback.php
+++ Pages/Callback.php
@@ -31,12 +31,13 @@
 
                         if(!$flickr->authenticate('write')) die('Laughing');
 
-                            // $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
+                            $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
                             $userName = $flickr->getOauthData(\DPZ\Flickr::USER_NAME);
                             $userFullName = $flickr->getOauthData(\DPZ\Flickr::USER_FULL_NAME);
 
                           $result['fullname'] = $userFullName;
                           $result['username'] = $userName;
+			  $result['nsid'] = $userNsid;
 
                          $result['token'] = $flickr->getOauthData('oauth_access_token');
                         $result['secret'] = $flickr->getOauthData('oauth_access_token_secret');
@@ -44,7 +45,7 @@
 
                     if (!empty($result['token'])) {
                          $user = \Idno\Core\Idno::site()->session()->currentUser();
-                         $user->flickr[$result['username']] = array('access_token' => $result['token'], 'secret' => $result['secret'], 'username' => $result['fullname']);
+                         $user->flickr[$result['username']] = array('access_token' => $result['token'], 'secret' => $result['secret'], 'username' => $result['fullname'], 'nsid' => $result['nsid']);
                          $user->save();
                          \Idno\Core\Idno::site()->logging()->log('Flickr user '.$result['username'].' authenticated.');
                     }
