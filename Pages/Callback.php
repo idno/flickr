a85bf2fbf32ab8e92566307be1426346356d6a79
C <cn@chrs.nl>
2018-10-22 13:22:17 +0000

    Code cleanup and improved error handling

--- Pages/Callback.php
+++ Pages/Callback.php
@@ -17,31 +17,28 @@
                 $this->gatekeeper(); // Logged-in users only
                 if ($flickr = \Idno\Core\Idno::site()->plugins()->get('Flickr')) {
 
-                    $api_key = \Idno\Core\Idno::site()->config()->flickr['apiKey'];
-                    $api_secret = \Idno\Core\Idno::site()->config()->flickr['secret'];
+                include_once dirname(__FILE__) . '/../external/DPZ/Flickr.php';
+                $flickr = new \DPZ\Flickr(
+                    \Idno\Core\Idno::site()->config()->flickr['apiKey'],
+                    \Idno\Core\Idno::site()->config()->flickr['secret'],
+                    \Idno\Core\Idno::site()->config()->getURL() . 'flickr/callback'
+                );
 
-                    $callback = \Idno\Core\Idno::site()->config()->getURL() . 'flickr/callback';
+                if (!$flickr->authenticate('write')) {
+                    \Idno\Core\Idno::site()->logging()->error('Failed to authenticate with Flickr API');
+                    $this->forward(\Idno\Core\Idno::site()->config()->getDisplayURL() . 'account/flickr/');
+                }
 
-                    require_once(dirname(__FILE__) . '/../external/DPZ/Flickr.php');
-                    $flickr = new \DPZ\Flickr(\Idno\Core\Idno::site()->config()->flickr['apiKey'],
-                                              \Idno\Core\Idno::site()->config()->flickr['secret'],
-                                              $callback);
+                    $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
+                    $userName = $flickr->getOauthData(\DPZ\Flickr::USER_NAME);
+                    $userFullName = $flickr->getOauthData(\DPZ\Flickr::USER_FULL_NAME);
 
-                    if(empty($_SESSION['faccess_oauth_token'])) {
-
-                        if(!$flickr->authenticate('write')) die('Laughing');
-
-                            $userNsid = $flickr->getOauthData(\DPZ\Flickr::USER_NSID);
-                            $userName = $flickr->getOauthData(\DPZ\Flickr::USER_NAME);
-                            $userFullName = $flickr->getOauthData(\DPZ\Flickr::USER_FULL_NAME);
-
-                          $result['fullname'] = $userFullName;
-                          $result['username'] = $userName;
-			  $result['nsid'] = $userNsid;
+                        $result['fullname'] = $userFullName;
+                        $result['username'] = $userName;
+                        $result['nsid'] = $userNsid;
 
                          $result['token'] = $flickr->getOauthData('oauth_access_token');
                         $result['secret'] = $flickr->getOauthData('oauth_access_token_secret');
-                    }
 
                     if (!empty($result['token'])) {
                          $user = \Idno\Core\Idno::site()->session()->currentUser();
